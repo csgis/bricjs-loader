35a41711375507fa13fc1d4f22cdc0dd335f0750
Víctor González <victor.gonzalez@geomati.co>
2017-11-22 15:08:31 +0100

    Remove webpack stuff; simply export a default load function

--- src/main.js
+++ src/main.js
@@ -1,25 +1,11 @@
-let fs = require('fs');
-let UglifyJS = require('uglify-js');
-
-const EXPORT_NAME = 'bricjs';
-const UGLIFY_OPTS = {
-  parse: {},
-  compress: false,
-  mangle: false,
-  output: {
-    ast: true,
-    code: false
-  }
-};
-
-export function load(app, config, modules, moduleDeps) {
+export default function (config, modules, moduleDeps) {
   let allPromises = [];
 
-  function process(env) {
+  function process(context) {
     let promises = {};
     let resolves = {};
 
-    let isEnabled = name => !env[name] || env[name].enabled !== false;
+    let isEnabled = name => !context[name] || context[name].enabled !== false;
     let enabledModules = Object.keys(modules).filter(isEnabled);
 
     // Create promises for all active modules
@@ -32,13 +18,8 @@ export function load(app, config, modules, moduleDeps) {
     // Load modules and resolve them
     enabledModules.forEach(name => {
       let callback = modules[name];
-      let opts = env[name];
-      let deps = [];
-      if (app.deps && app.deps[name]) {
-        deps = app.deps[name];
-      } else {
-        deps = moduleDeps[name] || [];
-      }
+      let opts = context[name];
+      let deps = moduleDeps[name] || [];
 
       let modulePromises = deps
         .map(d => promises[d])
@@ -60,54 +41,4 @@ export function load(app, config, modules, moduleDeps) {
   }
 
   return Promise.all(allPromises);
-}
-
-class Visitor {
-  visit(node) {
-    let found = node instanceof UglifyJS.AST_Lambda &&
-      node.name &&
-      node.name.name === EXPORT_NAME;
-    if (found) this.args = node.argnames.map(a => a.name).splice(1);
-    return found;
-  }
-}
-
-function getModuleDeps(loader, modules) {
-  let visitor = new Visitor();
-  let walker = new UglifyJS.TreeWalker(visitor.visit.bind(visitor));
-  let promises = [];
-
-  Object.keys(modules).forEach(function (name) {
-    let promise = new Promise(function (resolve, reject) {
-      loader.resolve(loader.context, modules[name], function (err, result) {
-        if (err) reject(err);
-
-        let contents = fs.readFileSync(result, 'UTF-8');
-        UglifyJS.minify(contents, UGLIFY_OPTS).ast.walk(walker);
-        resolve({
-          [name]: visitor.args
-        });
-      });
-    });
-    promises.push(promise);
-  });
-
-  return Promise.all(promises).then(values => Object.assign({}, ...values));
-}
-
-export default function (source) {
-  let modules = JSON.parse(source).modules;
-  let names = Object.keys(modules);
-  let imports = names.map(n => `import { ${EXPORT_NAME} as ${n} } from '${modules[n]}';`).join('\n');
-
-  let callback = this.async();
-  let code = deps => `
-${imports}
-const DEPS = ${JSON.stringify(deps)};
-${load.toString()}
-export default config => load(${source}, config, { ${names.join(',')} }, DEPS);`;
-
-  getModuleDeps(this, modules)
-    .then(deps => callback(null, code(deps)))
-    .catch(err => callback(err));
 }
--- test/main-test.js
+++ test/main-test.js
@@ -1,104 +1,21 @@
 debugger; // eslint-disable-line no-debugger
 
-import loader from '../src/main.js';
-import { load } from '../src/main.js';
+import load from '../src/main.js';
 import assert from 'assert';
-import sinon from 'sinon';
-import fs from 'fs';
-
-const SOURCE = {
-  'modules': {
-    'map': './map',
-    'layers': 'layers-dependency'
-  },
-  'deps': {
-    'layers': ['map']
-  }
-};
 
 let map = (opts) => opts;
 let layers = (opts, mymap) => { mymap.ten = 10; };
-let app = { deps: { layers: ['map'] } };
+let deps = { layers: ['map'] };
 
 describe('module', function () {
-  let cb;
-  let context;
-  let read;
-
-  beforeEach(function () {
-    context = {
-      async: () => cb,
-      resolve: (ctx, module, callback) => callback(null, '')
-    };
-  });
-
-  afterEach(function () {
-    read.restore();
-  });
-
-  function stubRead(value) {
-    read = sinon.stub(fs, 'readFileSync').callsFake(() => value);
-  }
-
-  it('imports modules', function (done) {
-    stubRead('module.exports = function bricjs(opts, a){}');
-    cb = function (e, ret) {
-      assert(ret.match(/import { bricjs as map } from '.\/map'/));
-      assert(ret.match(/import { bricjs as layers } from 'layers-dependency'/));
-      done();
-    };
-
-    loader.call(context, JSON.stringify(SOURCE));
-  });
-
-  it('adds load function', function (done) {
-    stubRead('module.exports = function bricjs(opts, a){}');
-    cb = function (e, ret) {
-      assert(ret.match(/function\s*load\s*\(.*\)/));
-      done();
-    };
-
-    loader.call(context, JSON.stringify(SOURCE));
-  });
-
-  it('exports function', function (done) {
-    stubRead('module.exports = function bricjs(opts, a){}');
-    cb = function (e, ret) {
-      assert(ret.match(/export default config => load\(.*, config, { map,layers }, DEPS\);/));
-      done();
-    };
-
-    loader.call(context, JSON.stringify(SOURCE));
-  });
-
-  it('exports DEPS', function (done) {
-    stubRead('module.exports = function bricjs(opts, dependency){}');
-    cb = function (e, ret) {
-      assert(ret.match(/const DEPS = {"map":\["dependency"\],"layers":\["dependency"\]};/));
-      done();
-    };
-
-    loader.call(context, JSON.stringify(SOURCE));
-  });
-
-  it('handles errors', function (done) {
-    context = {
-      async: () => function (e) {
-        assert(e);
-        done();
-      },
-      resolve: (ctx, module, callback) => callback('error')
-    };
-
-    loader.call(context, JSON.stringify(SOURCE));
-  });
-
   it('loads modules', function (done) {
     let config = {
       map: { boo: 2 }
     };
-    let test = () => assert.equal(10, config.map.ten);
-    load(app, [config], { map, layers }, app.deps).then(test).then(done, done);
+
+    load([config], { map, layers }, deps)
+      .then(() => assert.equal(10, config.map.ten))
+      .then(done, done);
   });
 
   it('disables modules', function (done) {
@@ -106,31 +23,25 @@ describe('module', function () {
       map: { boo: 2 },
       layers: { enabled: false }
     };
-    let test = () => assert(!config.map.ten);
-    load(app, [config], { map, layers }, app.deps).then(test).then(done, done);
-  });
 
-  it('ignores unnamed functions', function (done) {
-    stubRead('module.exports = function(opts, dependency){}');
-    cb = function (e, ret) {
-      assert(ret.match(/const DEPS = {};/));
-      done();
-    };
-
-    loader.call(context, JSON.stringify(SOURCE));
+    load([config], { map, layers }, deps)
+      .then(() => assert(!config.map.ten))
+      .then(done, done);
   });
 
   it('uses config as object', function (done) {
     let config = {
       map: { boo: 2 }
     };
-    let test = () => assert.equal(10, config.map.ten);
-    load(app, config, { map, layers }, app.deps).then(test).then(done, done);
+
+    load(config, { map, layers }, deps)
+      .then(() => assert.equal(10, config.map.ten))
+      .then(done, done);
   });
 
   it('throws error on invalid config', function () {
     try {
-      load(app, 'invalid_config', { map, layers }, app.deps);
+      load('invalid_config', { map, layers }, deps);
       assert.fail();
     } catch (e) {
       // ignore
